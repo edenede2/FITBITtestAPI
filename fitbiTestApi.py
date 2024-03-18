bc4823e95efb50062c051deb224fa29e0e940467
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-03-18 07:16:39 +0200

    Update fitbiTestApi.py

--- fitbiTestApi.py
+++ fitbiTestApi.py
@@ -21,30 +21,7 @@ def fetch_data(access_token, data_type, start_date, end_date):
     else:
         st.error(f"Failed to fetch data: {response.status_code}, {response.text}")
         return None
-# Function to fetch sleep data
-def get_sleep_data(access_token):
-    url = "https://api.fitbit.com/1.2/user/-/sleep/date/2024-03-17.json"
-    headers = {"Authorization": f"Bearer {access_token}"}
-    response = requests.get(url, headers=headers)
-    return response.json()
 
-# Function to fetch activity data
-def get_activity_data(access_token):
-    url = "https://api.fitbit.com/1/user/-/activities/date/2023-03-15.json"
-    headers = {"Authorization": f"Bearer {access_token}"}
-    response = requests.get(url, headers=headers)
-    return response.json()
-
-# Function to fetch data
-def fetch_data(access_token, data_type, start_date, end_date):
-    base_url = "https://api.fitbit.com/1.2/user/-/"
-    headers = {"Authorization": f"Bearer {access_token}"}
-    if data_type == 'Sleep':
-        url = f"{base_url}sleep/date/{start_date}/{end_date}.json"
-    else:  # Activity
-        url = f"{base_url}activities/steps/date/{start_date}/{end_date}.json"
-    response = requests.get(url, headers=headers)
-    return response.json()
 
 # Load access tokens from a file
 @st.cache_data
@@ -64,13 +41,13 @@ def load_tokens(file_path):
 st.title('Fitbit Data Explorer')
 
 # Load and select access token
-token_file_path = 'access_tokens.txt'  # Ensure this file exists
+token_file_path = 'access_tokens.txt'  
 tokens = load_tokens(token_file_path)
 selected_label = st.selectbox('Select a Watch:', list(tokens.keys()))
 selected_token = tokens[selected_label]
 
 # Select data type
-data_type = st.radio("Select Data Type:", ['Sleep', 'Activity', 'Sleep Levels', 'Heart Rate'])
+data_type = st.("Select Data Type:", ['Sleep', 'Activity', 'Sleep Levels', 'Heart Rate'])
 
 # Initialize default start and end dates as today's date, or choose your own defaults
 default_start_date = datetime.date.today() - datetime.timedelta(days=7)
